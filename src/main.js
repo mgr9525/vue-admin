2f20aff4bd92bbb6014e678e92c89efb9ce0df6c
LinskRuis.32 <mgr9525@qq.com>
2020-10-11 16:26:09 +0800

    jh main

--- src/main.js
+++ src/main.js
@@ -4,59 +4,19 @@ import App from './App'
 import ElementUI from 'element-ui'
 // import 'element-ui/lib/theme-default/index.css'
 import 'element-ui/lib/theme-chalk/index.css'
-import VueRouter from 'vue-router'
 import store from './vuex/store'
-import Vuex from 'vuex'
 //import NProgress from 'nprogress'
 //import 'nprogress/nprogress.css'
-import routes from './routes'
+import router from './router'
 import Mock from './mock'
 Mock.bootstrap();
 import 'font-awesome/css/font-awesome.min.css'
 import api from '@/api/apis'
 
 Vue.use(ElementUI)
-Vue.use(VueRouter)
-Vue.use(Vuex)
 
 //NProgress.configure({ showSpinner: false });
 
-const router = new VueRouter({
-  routes
-})
-
-import { getToken,removeToken } from '@/common/js/storage';
-router.beforeEach((to, from, next) => {
-  //NProgress.start();
-  if (to.path == '/login') {
-    removeToken();
-    next();
-    return;
-  }
-  // let token = getToken();
-  // if(!token||token==''){
-  //   next({ path: '/login' })
-  //   return;
-  // }
-  // if (to.path == '/') {
-  //   next({ path: '/models' })
-  //   return;
-  // }
-  if(store.state.userinfo.login==true){
-    next();
-    return;
-  }
-  store.dispatch('getLgInfo').then(()=>{
-    if(store.state.userinfo.login==true){
-      next();
-    }else{
-      next({ path: '/login' })
-    }
-  }).catch(err=>{
-    console.log('getLgInfo err:',err)
-    next({ path: '/login' })
-  });
-})
 
 //router.afterEach(transition => {
 //NProgress.done();
