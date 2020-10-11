407e64f21f80b4ca3c16f16cdc86818fcd8d3a7e
LinskRuis.32 <mgr9525@qq.com>
2020-10-11 20:20:53 +0800

    404+

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,11 +1,19 @@
 <template>
+<div class="mainDiv">
     <p class="page-container">404 page not found</p>
+    <p class="page-container">网页未找到</p>
+    <p><el-button type="text" @click="$router.push('/')">返回首页</el-button></p>
+</div>
 </template>
 
 <style lang="scss" scoped>
+.mainDiv{
+    margin-top: 50px;
+    text-align: center;
+}
     .page-container {
         font-size: 20px;
         text-align: center;
-        color: rgb(192, 204, 218);
+        color:#f06;
     }
 </style>
